fda90abf667500d721f3ed971cbe6c3862cb6e9b
Gabriella <128740583+morgadogs@users.noreply.github.com>
2023-11-10 10:19:09 -0300

    Change: save Monte Carlo simulation as matrix of returns (num_periods+1) x (num_trials)

--- src/portfoliorisk/portfoliorisk.py
+++ src/portfoliorisk/portfoliorisk.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 # May need adaptations according to the format received by finance and management portfolios libs
 
-def compute_covariances_and_means (portfolio):
+def compute_covariances_and_means (portfolio, type_period='months'):
     """
     Receives a portfolio.
     Returns an approximate covariance matrix of the portfolio
@@ -22,28 +22,29 @@ def monte_carlo_simulation (portfolio, file_path_simulation=None, file_path_port
     """
     Receives a portfolio, a file path to save the results of the simulation in npy, an amount of
     periods and the type of period (days, months or years), a file path to save the portfolio in
-    JSON, the number of periods to iterate and the type of periods (days, months or years).
+    JSON, the number of periods to iterate, the type of periods (days, months or years) and the
+    number of trials for each period for the Monte Carlo simulation.
     Returns an array of simulated returns and the portfolio in list format.
     """
     # data = blah blah blah
     df = pd.DataFrame(data)
-    returns_covariance_matrix, return_means = compute_covariances_and_means(portfolio)
+    returns_covariance_matrix, return_means = compute_covariances_and_means(portfolio, type_period)
 
     assets_values_at_last_date = df.iloc[-1:].to_numpy() # get the most recent values for each asset
     asset_weights = np.array([asset[1] for asset in portfolio])
     portfolio_value_at_last_date = assets_values_at_last_date * asset_weights
 
-    simulated_returns = np.zeros(num_trials)
+    simulated_returns = np.zeros((num_periods+1, num_trials))
     num_assets = len(portfolio)
+    simulated_returns[0] = np.repeat(np.sum(portfolio_value_at_last_date))
 
     for idx in range(num_trials): # run a Monte Carlo simulation
         simulated_portfolio_values = portfolio_value_at_last_date
-        for day in range(num_periods): # assume log returns are in a multivariate normal distribution
+        for period in range(1,num_periods+1): # assume log returns are in a multivariate normal distribution
             log_return_variations = np.random.multivariate_normal(returns_means, returns_covariance_matrix)
             period_log_returns = log_return_variations + np.log(simulated_portfolio_values)
             simulated_portfolio_values = np.exp(period_log_returns)
-        simulated_return = simulated_portfolio_values.sum()
-        simulated_returns[idx] = simulated_return
+            simulated_returns[period][idx] = np.sum(simulated_portfolio_values)
     
     if file_path_simulation is not None:
         if not file_path_simulation.endswith('.npy'):
@@ -60,21 +61,14 @@ def monte_carlo_simulation (portfolio, file_path_simulation=None, file_path_port
 
 def load_monte_carlo_simulation (portfolio, file_path, type_period='months'):
     """
-    Receives a portfolio or its file path in JSON and a file path of the simulation in npy.
+    Receives a portfolio and a file path of the simulation in npy.
     Returns an array of returns simulated by a Monte Carlo simulation, the portfolio and the type of period.
     """
-    if isinstance(portfolio, str):
-        if portfolio.endswith('.json'):
-            with open(portfolio, 'r') as json_file:
-                open_portfolio = json.load(json_file)
-        else:
-            raise ValueError(f'Not supported file path: {portfolio}. File must be in JSON format.')
-    else:
-        open_portfolio = portfolio
     if not file_path.endswith('.npy'):
         raise ValueError(f'Not supported file path: {file_path}. File must be in npy format.')
     simulated_returns = np.load(file_path)
-    return simulated_returns, open_portfolio, type_period   
+
+    return simulated_returns, portfolio, type_period   
 
 def portfolio_expected_return (portfolio, num_periods, type_period='months', simulation_path=None, num_trials=1000):
     """
@@ -84,27 +78,18 @@ def portfolio_expected_return (portfolio, num_periods, type_period='months', sim
     Returns an approximation of the expected value using a Monte Carlo simulation.
     """
     if simulation_path is not None:
-        simulated_returns = load_monte_carlo_simulation(portfolio, simulation_path, type_period)[0]
+        simulated_returns = load_monte_carlo_simulation(portfolio, simulation_path, type_period)[0][-1]
     else: # run a Monte Carlo simulation without saving it
-        simulated_returns = monte_carlo_simulation(portfolio, None, None, num_periods, type_period, num_trials)[0]
+        simulated_returns = monte_carlo_simulation(portfolio, None, None, num_periods, type_period, num_trials)[0][-1]
 
     return np.mean(simulated_returns)
 
-def portfolio_risk_index (portfolio):
+def portfolio_risk_index (portfolio, type_period='months'):
     """
     Receives a portfolio.
     Returns a risk index.
     """
-    if isinstance(portfolio, str):
-        if portfolio.endswith('.json'):
-            with open(portfolio, 'r') as json_file:
-                open_portfolio = json.load(json_file)
-        else:
-            raise ValueError(f'Not supported file path: {portfolio}. File must be in JSON format.')
-    else:
-        open_portfolio = portfolio
-
-    returns_covariance_matrix, _ = compute_covariances_and_means(open_portfolio)
+    returns_covariance_matrix, _ = compute_covariances_and_means(open_portfolio, type_period)
 
     asset_weights = np.array([asset[1] for asset in open_portfolio])
     asset_weights = asset_weights / asset_weights.sum()
@@ -125,5 +110,8 @@ def portfolio_percentiles (portfolio, percentiles=[5, 10, 50, 90, 100], num_peri
         simulated_returns = load_monte_carlo_simulation(portfolio, simulation_path, type_period)[0]
     else:
         simulated_returns = monte_carlo_simulation(portfolio, None, None, num_periods, type_period, num_trials)[0]
-    scores = sp.stats.scoreatpercentile(simulated_returns, percentiles)
+    percentiles = np.array(percentiles)
+    scores = np.zeros((num_periods+1, np.shape(percentiles)[0]))
+    for period in range(num_periods+1):
+        scores[period] = sp.stats.scoreatpercentile(simulated_returns[period], percentiles)
     return scores
